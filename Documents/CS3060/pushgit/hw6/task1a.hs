7e2087487babe3a69853ab50cae0b74c24c224e5
Nick Abell <105069583+Nick-Abell@users.noreply.github.com>
2022-10-24 18:28:23 -0400

    Delete Documents/CS3060/pushgit/hw6 directory

--- Documents/CS3060/pushgit/hw6/task1a.hs
+++
@@ -1,5 +0,0 @@
-bar :: Integer -> Integer -> Integer 
-
-bar x y = foldl (+) 0 [j^3 | j <- [x..y],(j `mod` 2) ==0]
-
-main = print (bar 5 9)
--- Documents/CS3060/pushgit/hw6/task1c.hs
+++
@@ -1,6 +0,0 @@
-shrtStrCount :: [String] -> Int
-
-
-shrtStrCount word = length (filter (\j -> length j < 4) word)
-
-main = print (shrtStrCount ["abcd", "de","fghtestwsd"])
